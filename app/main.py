a613658fe8b2d0ad91fb35a8af4099d486661a86
theyaya <panilag.althea123@gmail.com>
2025-12-19 03:08:35 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,25 @@
 from fastapi import FastAPI, Depends
-from sqlmodel import Session
+from sqlmodel import Session, select
 from .database import create_db, get_session
 from .models import Item
-from .crud import create_item, get_items
 
 app = FastAPI()
 
 @app.on_event("startup")
-def on_start():
+def on_startup():
     create_db()
 
 @app.get("/")
 def root():
-    return {"message": "API is running!"}
+    return {"message": "Backend running"}
 
 @app.post("/items")
-def post_item(item: Item, session: Session = Depends(get_session)):
-    return create_item(session, item)
+def create_item(item: Item, session: Session = Depends(get_session)):
+    session.add(item)
+    session.commit()
+    session.refresh(item)
+    return item
 
 @app.get("/items")
 def read_items(session: Session = Depends(get_session)):
-    return get_items(session)
+    return session.exec(select(Item)).all()
